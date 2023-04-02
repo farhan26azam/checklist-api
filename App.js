c0149a442f227b054c5ea77c1121c71ba8f4e31f
BSCE20004_Farhan <73163937+farhan26azam@users.noreply.github.com>
2023-04-02 19:22:43 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import List from './components/List';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path='/' element={<List />}/> 
+       {/* <Route path="/" element={<Signup/>}  /> */}
+       {/* <Route path="/Login" element={<Login/>}/> */}
+     </Routes>
+
+
+
+    </>
+    
+  );
+}
+
+export default App;
